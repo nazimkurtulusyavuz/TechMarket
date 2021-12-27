e9c7209b90d9860984ada85b8e5d6e9510c8433b
nazim yavuz <nazimkurtulusyavuz@gmail.com>
2021-12-27 11:18:13 +0300

    pagination

--- src/Web/Models/PaginationInfoViewModel.cs
+++ src/Web/Models/PaginationInfoViewModel.cs
@@ -25,5 +25,42 @@ namespace Web.Models
         public int TotalPages => (int)Math.Ceiling((double)TotalItems / ItemsPerPage);
         public bool HasPrevious => CurrentPage > 1;
         public bool HasNext => CurrentPage < TotalPages;
+        public int[] PageNumbers => Pagination(CurrentPage, TotalPages);
+        private static int[] Pagination(int current, int last)
+        {
+            int delta = 1;
+            int left = current - delta;
+            int right = current + delta + 1;
+            var range = new List<int>();
+            var rangeWithDots = new List<int>();
+            int? l = null;
+
+            for (var i = 1; i <= last; i++)
+            {
+                if (i == 1 || i == last || i >= left && i < right)
+                {
+                    range.Add(i);
+                }
+            }
+
+            foreach (var i in range)
+            {
+                if (l != null)
+                {
+                    if (i - l == 2)
+                    {
+                        rangeWithDots.Add(l.Value + 1);
+                    }
+                    else if (i - l != 1)
+                    {
+                        rangeWithDots.Add(-1);
+                    }
+                }
+                rangeWithDots.Add(i);
+                l = i;
+            }
+
+            return rangeWithDots.ToArray();
+        }
     }
 }
